f1c1333232a733c9cb9652326e74c96ba1bb7756
Erika Heidi <er@mobinvest.com>
2013-04-23 14:55:10 +0200

    removing old conf folder

--- conf/config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * The file /app/config.php should not be kept in source control, as it likes
- * contains settings specific to the environment that the site is running on.
- * This file should contain a boilerplate of the contents of that file, with
- * any environment settings that an application will need to configure such
- * as the MySQL connection details or any api credentials.
- */
